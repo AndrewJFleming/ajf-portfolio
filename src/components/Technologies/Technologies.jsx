be1825e907e597c300c679e3404412e00888aa63
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-03-15 16:02:29 -0700

    Create Technologies component with Technology components.
    -Prepare SVG files for Technology components.
    -Create data.js file with data to be used for website.

---
+++ src/components/Technologies/Technologies.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { Container, Row, Col } from "react-bootstrap";
+import { technologies } from "../../data.js";
+
+import "./Technologies.css";
+import Technology from "./Technology/Technology";
+
+const Technologies = () => {
+  return (
+    <div className="techs-wrapper py-5">
+      <Container>
+        <Row>
+          <Col className="text-center">
+            <h3>MERN stack with a focus on Frontend Development</h3>
+            <p>
+              As a React developer, I’m focused on frontend development.
+              However, having learned the full MERN stack, I also build RESTful
+              APIs.
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          {technologies.map((tech, index) => (
+            <Technology key={tech.id} technology={tech} />
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+};
+export default Technologies;
