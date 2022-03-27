33584bb1d41d85d4971072fd9076bb20ecb023fe
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-03-27 15:15:20 -0700

    Remove MyProjects page component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react'
-import { HashRouter, Switch, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
 import TopNav from "./components/TopNav/TopNav";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import SingleProject from "./pages/SingleProject/SingleProject";
 
 function App() {
   return (
     <div className="app-content-wrapper">
-    <div className="app-content">
-
-      <HashRouter>
-        <TopNav />
-        <Switch>
-          <Route exact path="/">
-            <Home/>
-          </Route>
-        </Switch>
-        <Footer />
-      </HashRouter>
-    </div>
+      <div className="app-content">
+        <Router>
+          <TopNav />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/projects/:projectSlug">
+              <SingleProject />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
+      </div>
     </div>
   );
 }
