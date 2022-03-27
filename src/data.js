89b5542a44bd010a426b5c5b0145b47b554791de
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-03-27 15:16:13 -0700

    Update data.js.

--- src/data.js
+++ src/data.js
@@ -1,126 +1,314 @@
-  export const technologies = [
-    {
-      id: "t1",
-      title: "Seamless User Experience",
-      desc: "I follow the AGILE methodology in developing my web app user interfaces to ensure that the user experience is smooth, intuitive and error-free.",
-      icon: "fas fa-user-circle",
-      img: "https://gist.githubusercontent.com/AndrewJFleming/b22a077d3a052f89282db78f04820199/raw/d3512508b85abcf77183f5a99728d7127c7996f5/illust-5.svg"
-    },
-    {
-      id: "t2",
-      title: "Full CRUD functionality",
-      desc: "All of my MERN stack web apps allow users authenticated with JSON web tokens to create, update and delete MongoDB data.",
-      icon: "fal fa-chart-network",
-      img: "https://gist.githubusercontent.com/AndrewJFleming/0287957b81e3ed02e7e36a1f3717632a/raw/6fe0d7561c039199e0e579079f8d2263110ef9e1/illust-4.svg"
-    },
-    {
-      id: "t3",
-      title: "Stylish Visual Aesthetic",
-      desc: "My illustration and graphic design background gives me insight into creating visually appealing web apps. I utilize my color theory and value composition expertise to ensure a professional-looking product.",
-      icon: "fas fa-palette",
-      img: "https://gist.githubusercontent.com/AndrewJFleming/092aaafcb47002d105d6286b43b516fe/raw/c5e208cdc96e34b51e1670eb69f8135ddd5672e3/illust-2.svg"
-    },
-  ];
+export const technologies = [
+  {
+    id: "t1",
+    title: "Seamless User Experience",
+    desc: "I follow the AGILE methodology in developing my web app user interfaces to ensure that the user experience is smooth, intuitive and error-free.",
+    icon: "fas fa-user-circle",
+    img: "https://gist.githubusercontent.com/AndrewJFleming/b22a077d3a052f89282db78f04820199/raw/d3512508b85abcf77183f5a99728d7127c7996f5/illust-5.svg",
+  },
+  {
+    id: "t2",
+    title: "Full CRUD functionality",
+    desc: "All of my MERN stack web apps allow users authenticated with JSON web tokens to create, update and delete MongoDB data.",
+    icon: "fal fa-chart-network",
+    img: "https://gist.githubusercontent.com/AndrewJFleming/0287957b81e3ed02e7e36a1f3717632a/raw/6fe0d7561c039199e0e579079f8d2263110ef9e1/illust-4.svg",
+  },
+  {
+    id: "t3",
+    title: "Stylish Visual Aesthetic",
+    desc: "My illustration and graphic design background gives me insight into creating visually appealing web apps. I utilize my color theory and value composition expertise to ensure a professional-looking product.",
+    icon: "fas fa-palette",
+    img: "https://gist.githubusercontent.com/AndrewJFleming/092aaafcb47002d105d6286b43b516fe/raw/c5e208cdc96e34b51e1670eb69f8135ddd5672e3/illust-2.svg",
+  },
+];
 
-  export const projectRows = [
-    {
-      id: "pr1",
-      title: "MERN Stack Project Highlights",
-      desc: "I’ve built my most recent web apps using the MERN stack (MongoDB, Express.js, React.js and Node.js). These apps deliver all the features expected by user of the modern web; they feature create, read, update and delete functionality.",
-      img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
-      type: "MERN",
-      projects: [
-        {
-          id: "p1",
-          title: "Embossed React Ecommerce",
-          desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",
-          img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
-          type: "MERN",
-        },
-        {
-          id: "p2",
-          title: "CritiqueMe",
-          desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",      img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
-          img: "https://live.staticflickr.com/65535/51939860342_9a31bf1be2_o.png",
-          type: "MERN",
-        },
-        {
-          id: "p3",
-          title: "Purgation React Blog",
-          desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",      img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
-          img: "https://live.staticflickr.com/65535/51940850061_7b021796b6_o.png",
-          type: "MERN",
-        }],
-    },
-    {
-      id: "pr2",
-      title: "More React.js Projects",
-      desc: "Explore more projects built using the React.js framework.",
-      img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
-      type: "React",
-      projects: [{
-        id: "4",
+export const projectRows = [
+  {
+    id: "pr1",
+    title: "MERN Stack Project Highlights",
+    desc: "I’ve built my most recent web apps using the MERN stack (MongoDB, Express.js, React.js and Node.js). These apps deliver all the features expected by user of the modern web; they feature create, read, update and delete functionality.",
+    img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
+    type: "mern",
+    projects: [
+      {
+        id: "p1",
+        title: "Embossed React Ecommerce",
+        brief:
+          "The Embossed React Ecommerce app is an online shop app built with the MERN stack. The repo contains the client, admin and server directories for the project. Users can browse for and add products to carts and the admin dashboard is a fully CRUD app for managing the database.",
+        fullDesc: "",
+        img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
+        type: "mern",
+        slug: "embossed-react-ecommerce",
+        gitHub: "https://github.com/AndrewJFleming/embossed-react-ecommerce",
+        demo: "https://embossed-react-ecommerce.netlify.app/#/",
+        screenshots: [
+          {
+            img: "https://live.staticflickr.com/65535/51949490275_e842e935f2_o.png",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51948958498_b5cb07a1d9_o.png",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51949490295_25ed5a3698_o.png",
+            caption: "",
+          },
+        ],
+      },
+      {
+        id: "p2",
+        title: "CritiqueMe",
+        brief:
+          "CritiqueMe users can browse images posted by other artists and, if they've registered an account, can post critiques/comments. Registered users can also post their own images to the homepage to have them critiqued by other users.",
+        fullDesc: "",
+        img: "https://live.staticflickr.com/65535/51939860342_9a31bf1be2_o.png",
+        type: "mern",
+        slug: "critique-me",
+        gitHub: "https://github.com/AndrewJFleming/critique-me",
+        demo: "https://critique-me.netlify.app/posts",
+        screenshots: [
+          {
+            img: "https://live.staticflickr.com/65535/51948958373_1fbd9a1a76_o.png",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51949490100_f4768181e5_o.png",
+            caption: "",
+          },
+        ],
+      },
+      {
+        id: "p3",
+        title: "Purgation React Blog",
+        brief:
+          "Purgation is a blog built with the MERN stack where users can register an account and post articles. Only the original post creator can update or delete posts.",
+        fullDesc: "",
+        img: "https://live.staticflickr.com/65535/51940850061_7b021796b6_o.png",
+        type: "mern",
+        slug: "purgation-react-blog",
+        gitHub: "https://github.com/AndrewJFleming/purgation-react-blog",
+        demo: "https://purgation-react-blog.netlify.app/",
+        screenshots: [
+          {
+            img: "https://live.staticflickr.com/65535/51949490165_337c94e624_o.png",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51949490135_df556f6db3_o.png",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51949490120_e38ca7efd9_o.png",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51948958403_4afd6cf822_o.png",
+            caption: "",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "pr2",
+    title: "More React.js Projects",
+    desc: "Explore more projects built using the React.js framework.",
+    img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
+    type: "react",
+    projects: [
+      {
+        id: "p4",
         title: "Embossed Dummy Data Admin",
-        desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",      img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
+        brief:
+          "This is a version of the Embossed admin app (the project above) that uses dummy-data instead of MongoDB database data. There are no api requests in this version of the project.",
+        fullDesc: "",
         img: "https://live.staticflickr.com/65535/51939860362_cec01912ae_o.png",
-        type: "React",
+        type: "react",
+        slug: "embossed-dummy-data-admin",
+        gitHub: "https://github.com/AndrewJFleming/embossed-dummy-data-admin",
+        demo: "https://embossed-react-ecommerce-admin.netlify.app/",
+        screenshots: [
+          {
+            img: "https://live.staticflickr.com/65535/51950845594_e193e2a845_o.png",
+            caption:
+              "Admin homepage displaying latest user data added, currently active sales and the history of created carts.",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51950612863_da585b6223_o.png",
+            caption:
+              "Ecommerce data table example (listing out product data in this example).",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51949556252_91f0a2d71a_o.png",
+            caption:
+              "Create document page example (displaying the new product in this example).",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51949556302_b775d57d43_o.png",
+            caption:
+              "Edit cart page where admins can manage the products belonging to a particular cart.",
+          },
+        ],
       },
       {
         id: "p5",
         title: "Portraits of Iowa",
-        desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",      img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
-        img: "https://live.staticflickr.com/65535/51941478440_4c3566dcae_o.png",
-        type: "React",
+        brief:
+          "Public artist John Fleming commissioned a website for Portraits of Iowa, the winning project in a Cedar Rapids, Iowa, art installation competition. My client’s installation involved gathering hundreds of photo submissions from Cedar Rapids residents that would then be assembled into a hanging gallery/sculpture.",
+        fullDesc: "",
+        img: "https://live.staticflickr.com/65535/51939932377_e83495dd4b_o.png",
+        type: "react",
+        slug: "portraits-of-iowa",
+        gitHub: "",
+        demo: "",
+        screenshots: [
+          {
+            img: "https://live.staticflickr.com/65535/51950523721_6cc5cf46d0_o.jpg",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51950600548_650b1a492a_o.jpg",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51950523706_4a83593281_o.jpg",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51949543857_8de00ca5b1_o.jpg",
+            caption: "",
+          },
+        ],
       },
       {
         id: "p6",
         title: "Jane Kazmier Counseling Site",
-        desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",      img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
-        img: "https://live.staticflickr.com/65535/51941478455_f90b531913_o.png",
-        type: "React",
-      }],
-    },
-    {
-      id: "pr3",
-      title: "Custom WordPress Themes",
-      desc: "Explore custom WordPress themes designed for professional artists in Seattle, WA.",
-      img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
-      type: "WP",
-      projects: [{
+        brief:
+          "Single page website built using React.js and the Materialize CSS library for Jane Kazmier’s Spiritual Direction practice.",
+        fullDesc:
+          "I designed a single page website for Jane Kazmier’s Spiritual Direction practice. She was interested in using a parallax image component and we decided to implement that in the welcome section. The website was produced using React.js and the Materialize CSS library.",
+        img: "https://live.staticflickr.com/65535/51940960503_179cd40f99_o.png",
+        type: "react",
+        slug: "j-k-counseling-site",
+        gitHub: "",
+        demo: "http://janekfleming.com/",
+        screenshots: [
+          {
+            img: "https://live.staticflickr.com/65535/51949556222_79029d73c5_o.png",
+            caption: "",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "pr3",
+    title: "Custom WordPress Themes",
+    desc: "Explore custom WordPress themes designed for professional artists in Seattle, WA.",
+    img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
+    type: "wp",
+    gitHub: "",
+    demo: "",
+    projects: [
+      {
         id: "p7",
         title: "John Fleming Portfolio Site",
-        desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",      img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
+        brief:
+          "A custom WP theme designed to feature John Fleming's extensive art/architecture portfolio. Thumbnails are presented in a masonry style and single post pages use the Magnific Popup plugin to display associated gallery images.",
+        fullDesc: "",
         img: "https://live.staticflickr.com/65535/51941464920_b9aa70b768_o.png",
-        type: "WP",
+        type: "wp",
+        slug: "j-f-portfolio-site",
+        gitHub: "https://github.com/AndrewJFleming/johnflemingartist",
+        demo: "https://www.johnflemingartist.com/wp/",
+        related: [],
+        screenshots: [
+          {
+            img: "https://live.staticflickr.com/65535/51951130450_52c75bb4bd_o.png",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51950600593_20ff696d98_o.png",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51950832924_279a756d15_o.png",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51950833034_798d237c00_o.png",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51949543947_354c01830a_o.png",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51950832954_b801713339_o.png",
+            caption: "",
+          },
+        ],
       },
       {
         id: "p8",
         title: "Amanda Sargent Artist Portfolio",
-        desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",      img: "https://raw.githubusercontent.com/AndrewJFleming/embossed-react-ecommerce/main/client/src/images/logo.png",
+        brief:
+          "A custom WP theme designed to feature Amanda Sargent's art portfolio. Visitors can browse her portfolio by category and inquire about the price and availability of specific pieces by email.",
+        fullDesc: "",
         img: "https://live.staticflickr.com/65535/51941464935_8e91bf717b_o.png",
-        type: "WP",
-      }],
-    },
-  ];
+        type: "wp",
+        slug: "a-s-artist-portfolio",
+        gitHub: "https://github.com/AndrewJFleming/amandasargentartist",
+        demo: "https://amandasargentartist.com/",
+        screenshots: [
+          {
+            img: "https://live.staticflickr.com/65535/51950832809_964c295a1f_o.png",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51950832779_99fc12d487_o.png",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51949543987_e35e26bf54_o.png",
+            caption: "",
+          },
+          {
+            img: "https://live.staticflickr.com/65535/51950523676_cc8d28b280_o.png",
+            caption: "",
+          },
+        ],
+      },
+    ],
+  },
+];
 
-  export const faqItems = [
-    {
-      id: "q1",
-      question: "Can I phone or email you?",
-      answer: "You can reach me on my contact page or by calling me at 1.206.471.8342.",
-      isDefaultOpen: true,
-    },
-    {
-      id: "q2",
-      question: "What kinds of apps/sites do you have experience with?",
-      answer: "I’ve built the following kinds of sites from scratch:",
-      list: ["Artist portfolio sites","Blogs (custom WP themes and React.js web app)","Ecommerce web apps"],
-      isDefaultOpen: true,
-    },
-    {
-      id: "q3",
-      question: "Do you have experience with any global state management technologies?",
-      answer: "I’ve built web apps with Redux: checkout Embossed React Ecommerce as an example.",
-      isDefaultOpen: false,
-    },
-  ];
+export const faqItems = [
+  {
+    id: "q1",
+    question: "Can I phone or email you?",
+    answer:
+      "You can reach me on my contact page or by calling me at 1.206.471.8342.",
+    isDefaultOpen: true,
+  },
+  {
+    id: "q2",
+    question: "What kinds of apps/sites do you have experience with?",
+    answer: "I’ve built the following kinds of sites from scratch:",
+    list: [
+      "Artist portfolio sites",
+      "Blogs (custom WP themes and React.js web app)",
+      "Ecommerce web apps",
+    ],
+    isDefaultOpen: true,
+  },
+  {
+    id: "q3",
+    question:
+      "Do you have experience with any global state management technologies?",
+    answer:
+      "I’ve built web apps with Redux: checkout Embossed React Ecommerce as an example.",
+    isDefaultOpen: false,
+  },
+];
