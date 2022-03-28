e27065ab2a550c0115cdbdabda15f7a26a0091ae
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-03-28 13:58:50 -0700

    Add Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,30 +2,41 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./Footer.css";
-import { Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 const Footer = () => {
   return (
     <div className="footer-wrapper">
       <Container>
         <div className="footer-top-wrapper">
+          <ul className="footer-top-list">
+            <li className="footer-top-list-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="footer-top-list-item">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="footer-top-list-item">
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-middle-wrapper">
           <p>
             <span>Copyright © 2022</span> · Andrew J Fleming
           </p>
         </div>
         <div className="footer-bottom-wrapper">
-          <ul className="footer-list">
-            <li className="footer-list-item">
-              <Link>Home</Link>
+          <ul className="footer-bottom-list">
+            <li className="footer--bottom-list-item">
+              <a href="https://github.com/AndrewJFleming/AndrewJFleming">
+                GitHub Profile
+              </a>
             </li>
-            <li className="footer-list-item">
-              <Link>Portfolio</Link>
-            </li>
-            <li className="footer-list-item">
-              <Link>About</Link>
-            </li>
-            <li className="footer-list-item">
-              <Link>Contact</Link>
+            <li className="footer--bottom-list-item">
+              <a href="https://www.linkedin.com/in/andrew-j-fleming-web-dev/">
+                LinkedIn Profile
+              </a>
             </li>
           </ul>
         </div>
