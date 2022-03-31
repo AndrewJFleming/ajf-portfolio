058da5b34e6bc42b1136d29b11c163e7f346f3bc
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-03-31 10:25:18 -0700

    Add ContactMe component to Home page component.
    ContactMe component serves as a divider to break up Home page component's series of components.

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { Container, Row, Col } from "react-bootstrap";
 import "./ContactMe.css";
@@ -8,11 +9,13 @@ const ContactMe = () => {
     <div className="contactMe-wrapper">
       <Container>
         <Row className="p-3">
-          <Col md="8" className="text-center">
+          <Col md="8" className="text-center contact-cta">
             <h2>Contact me today</h2>
           </Col>
           <Col md="4" className="text-center">
-            <button>Get in touch</button>
+            <Link to="/contact">
+              <button>Get in touch</button>
+            </Link>
           </Col>
         </Row>
       </Container>
