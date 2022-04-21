8c3faacc1b48c9eeda0f45628e1d0314febd059c
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-04-21 12:40:05 -0700

    Add html support for FAQItemAnswer text content.

--- src/components/FAQ/FAQItem/FAQItemAnswer/FAQItemAnswer.jsx
+++ src/components/FAQ/FAQItem/FAQItemAnswer/FAQItemAnswer.jsx
@@ -5,11 +5,14 @@ import "./FAQItemAnswer.css";
 const FAQItemAnswer = ({ answerList, answer }) => {
   return (
     <div className="faq-a-wrapper ">
-      <p className="faq-a">{answer}</p>
+      <p className="faq-a" dangerouslySetInnerHTML={{ __html: answer }}></p>
       {answerList && (
         <ul className="answer-list">
           {answerList.map((listItem) => (
-            <li key={listItem.id}>{listItem.content}</li>
+            <li
+              key={listItem.id}
+              dangerouslySetInnerHTML={{ __html: listItem.content }}
+            ></li>
           ))}
         </ul>
       )}
